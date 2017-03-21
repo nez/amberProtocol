a51cdc342e069ae69c328429ba0704fec64debc8
lromang <luis.roangarci@gmail.com>
2017-03-21 10:43:40 -0600

    sql

---
+++ sql/db.sql
@@ -0,0 +1,103 @@
+drop database if exists amber;
+create database amber;
+
+drop user if exists auser;
+create user auser with password 'test';
+
+grant all privileges on database amber to auser;
+
+\c amber;
+set role auser;
+
+/* Dependencias */
+drop table if exists dependencias cascade;
+create table dependencias (
+    id serial primary key,
+    nombre text,
+    direccion_calle text,
+    direccion_numero_int text,
+    direccion_numero_ext text,
+    direccion_colonia text,
+    direccion_localidad text,
+    direccion_municipio text,
+    direccion_ciudad text,
+    direccion_estado text,
+    direccion_pais text
+);
+
+/* Usuarios */
+drop table if exists usuarios cascade;
+create table usuarios(
+    id serial primary key,
+    usuario text,
+    contrasena text,
+    email text,
+    nombres text,
+    apellido_paterno text,
+    apellido_materno text,
+    rfc text,
+    direccion_calle text,
+    direccion_numero_int text,
+    direccion_numero_ext text,
+    direccion_colonia text,
+    direccion_localidad text,
+    direccion_municipio text,
+    direccion_ciudad text,
+    direccion_estado text,
+    direccion_pais text,
+    permiso_alerta boolean,
+    permiso_administrador boolean,
+    permiso_tablero boolean,
+    id_dependencia integer references dependencias(id)
+);
+
+insert into usuarios ("usuario","contrasena","nombres","apellido_paterno","apellido_materno","permiso_alerta","permiso_administrador", "permiso_tablero") values
+('admin','$2a$10$DmxbjTLBYDdcha8qlXpsaOyUqkJ0BAQ3Q4EIyMtr5HLXm6R0gSvbm','Administrador','','', true, true, true);
+
+/* Alertas */
+drop table if exists alertas cascade;
+create table alertas(
+    id serial primary key,
+    id_usuario integer references usuarios(id),
+    sent date,
+    status text,
+    msgType text,
+    source text
+);
+
+/* Alert Info */
+drop table if exists infos cascade;
+create table infos(
+    id serial primary key,
+    id_alert integer references alertas(id),
+    event text,
+    responseType text,
+    urgency text,
+    severity text,
+    certainty text,
+    effective date,
+    headline text,
+    description text
+    /* Area geografical info */
+);
+
+/* Codes */
+drop table if exists codes;
+create table codes(
+    id serial primary key,
+    id_infos integer references infos(id),
+    code_key text,
+    code_val  text
+);
+
+/* Parameters */
+drop table if exists codes;
+create table codes(
+    id serial primary key,
+    id_infos integer references infos(id),
+    param_key text,
+    param_val  text
+);
+
+
+
