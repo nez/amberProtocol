b520d4428123c425cbaf52755828c4ea6ceeb3fb
lromang <luis.roangarci@gmail.com>
2017-04-27 18:22:52 -0500

    alert look up

--- public/javascripts/xml.js
+++ public/javascripts/xml.js
@@ -4,3 +4,27 @@
 $('#xml').click(function(){
     alert($(this).data('action'));
 })
+
+$('#alerts_datepicker1').datetimepicker({
+    format: 'YYYY-MM-DD',
+    defaultDate: new Date().setDate(new Date().getDate() - 1)
+});
+$('#alerts_datepicker2').datetimepicker({
+    format: 'YYYY-MM-DD',
+    defaultDate: new Date().setDate(new Date().getDate())
+});
+$(document).find('#find').submit(function(e){
+    $(document).find('#search_results').load('/alerts/results', $(this).serializeArray(), function(){
+        $('#search_results').find('.list-group-item').click(function(){
+            $(document).find('#modal_content').load('/alert/edit', {id: $(this).data('alerts_id')}, function(){
+                $(document).find('form').submit(function(e){
+                    $.post('/alert/update', $(this).serializeArray()).done(function(data){
+                        alert(data.message);
+                    });
+                    e.preventDefault();
+                });
+            });
+        });
+    });
+    e.preventDefault();
+});
