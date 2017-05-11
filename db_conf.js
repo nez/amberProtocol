eee4753b088539bd1342b97dcc4ddf2627e7ce7c
Mario Torres <mario291187@gmail.com>
2017-05-10 20:41:02 -0500

    gosh!

--- db_conf.js
+++ db_conf.js
@@ -3,7 +3,7 @@ pgp = require('pg-promise')(options);
 
 const config = {
     host: process.env.DB_HOST || 'localhost',
-    port: process_env.DB_PORT || '5432',
+    port: process.env.DB_PORT || '5432',
     database: process.env.DB_NAME  || 'amber',
     user: process.env.DB_USER || 'auser',
     password: process.env.DB_PASSWORD || 'test'
